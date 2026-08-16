18763e30cd8387f4a803c8469ff13cc685587fba
Christian <fulldivegames@gmail.com>
2026-08-16 17:05:46 -0400

    Declare held Fire Flower display list

--- src/game/rendering_graph_node.c
+++ src/game/rendering_graph_node.c
@@ -35,6 +35,7 @@
 #include "vr_hand_interaction.h"
 #include "hardcoded.h"
 #include "levels/menu/header.h"
+#include "actors/common1.h"
 #include "actors/mario/geo_header.h"
 #include "behavior_data.h"
 #include "object_helpers.h"
