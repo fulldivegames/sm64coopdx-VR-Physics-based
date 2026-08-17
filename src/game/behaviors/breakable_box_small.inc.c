3a0c9070e665573c29dc32b41eb44c2516a897b1
Christian <fulldivegames@gmail.com>
2026-08-17 07:23:34 -0400

    Tune Fire Flower box odds and free right-hand charging

--- src/game/behaviors/breakable_box_small.inc.c
+++ src/game/behaviors/breakable_box_small.inc.c
@@ -54,7 +54,7 @@ void small_breakable_box_act_move(void) {
             vr_special_moves_spawn_fire_flower_chance(
                 o,
                 &gMarioStates[0],
-                0.30f
+                0.25f
             );
         spawn_mist_particles();
         spawn_triangle_break_particles(20, 138, 0.7f, 3);
--- src/game/vr_hand_interaction.c
+++ src/game/vr_hand_interaction.c
@@ -4138,7 +4138,7 @@ static bool vr_hand_interaction_attack_object(
         vr_special_moves_spawn_fire_flower_chance(
             object,
             mario,
-            0.30f
+            0.25f
         );
     }
 
@@ -5157,8 +5157,7 @@ static bool vr_special_moves_update_fireball_hand(
         sVrGripPressed[VR_CONTROLLER_RIGHT];
     const bool canCharge = vr_special_moves_fire_flower_active() &&
         !handBusy && gripPressed &&
-        mario != NULL && mario->heldObj == NULL &&
-        sVrPhysicalClimbType == VR_PHYSICAL_CLIMB_NONE;
+        mario != NULL;
     const bool chargingInput = triggerPressed && canCharge;
     const bool wasCharging = sVrFireballChargeFrames > 0 ||
         sVrFireballChargeObject != NULL;
@@ -5562,7 +5561,7 @@ void vr_hand_interaction_update(struct MarioState* mario) {
         const bool handIsHoldingVrItem = handIsHoldingCap;
         const bool fireballHandBusy =
             handIsHoldingVrItem ||
-            sVrTrackedHeldObject != NULL ||
+            (sVrTrackedHeldGripMask & (u8)(1U << hand)) != 0 ||
             sVrPhysicalClimbHands[hand] ||
             sVrTrackedHootHand == hand ||
             sVrTrackedAnchorHand == hand ||
