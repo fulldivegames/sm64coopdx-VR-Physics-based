8fc5806146bb56cc3e52b984696468ad39c67873
Christian <fulldivegames@gmail.com>
2026-08-17 06:34:29 -0400

    Tune Fire Flower spawning and hand placement

--- src/game/behaviors/jumping_box.inc.c
+++ src/game/behaviors/jumping_box.inc.c
@@ -75,7 +75,7 @@ void bhv_jumping_box_loop(void) {
             vr_special_moves_spawn_fire_flower_chance(
                 o,
                 nearest_mario_state_to_object(o),
-                0.25f
+                0.30f
             );
         create_sound_spawner(SOUND_GENERAL_BREAK_BOX);
         obj_explode_and_spawn_coins(46.0f, fireFlower ? 0 : 1);
