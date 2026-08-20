c3d8d712a598d4543f0852483355f322a4e68fa3
Christian <fulldivegames@gmail.com>
2026-08-20 14:18:46 -0400

    Fix anchored HUD icons and held actor rendering

--- src/game/ingame_menu.h
+++ src/game/ingame_menu.h
@@ -143,6 +143,7 @@ void create_dl_translation_matrix(s8 pushOp, f32 x, f32 y, f32 z);
 void create_dl_rotation_matrix(s8 pushOp, f32 a, f32 x, f32 y, f32 z);
 void create_dl_ortho_matrix(void);
 void create_dl_vr_ui_matrix(void);
+void create_dl_vr_hud_matrix(void);
 void render_screen_texture_rectangle(s16 x, s16 y, s16 width, s16 height,
                                      s16 textureWidth, s16 textureHeight);
 void render_generic_char(u8 c);
