7e0648e93dcb372b73d81601c6e492f9c7725c1f
Christian <fulldivegames@gmail.com>
2026-08-22 15:47:55 -0400

    Fix true first person flip orientation

--- src/game/mario.c
+++ src/game/mario.c
@@ -694,6 +694,12 @@ s16 vr_get_first_person_view_yaw(void) {
     );
 }
 
+s16 vr_get_first_person_headset_yaw(void) {
+    return vr_get_first_person_aim_yaw_from_head_yaw(
+        vr_first_person_facing_yaw_offset()
+    );
+}
+
 static bool vr_get_first_person_direction(
     struct MarioState* m,
     Vec3f direction,
